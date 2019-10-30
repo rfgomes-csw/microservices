ecfe6d1826e8a888b5e64b44a7f38549214a505f
Ricardo Gomes <ipsiart@gmail.com>
2019-10-30 17:31:03 +0000

    adds hystrix

--- license-service/src/main/kotlin/com/ricardo/licenseservice/DummyController.kt
+++ license-service/src/main/kotlin/com/ricardo/licenseservice/DummyController.kt
@@ -1,5 +1,6 @@
 package com.ricardo.licenseservice
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import com.ricardo.licenseservice.organization.OrganizationDiscoveryClient
 import com.ricardo.licenseservice.organization.OrganizationFeignClient
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,5 +10,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DummyController(@Autowired val organizationDiscoveryClient: OrganizationFeignClient) {
     @GetMapping("/")
+    @HystrixCommand
     fun getEurekaClient() = organizationDiscoveryClient.getOrganization()
 }
--- license-service/src/main/kotlin/com/ricardo/licenseservice/LicenseServiceApplication.kt
+++ license-service/src/main/kotlin/com/ricardo/licenseservice/LicenseServiceApplication.kt
@@ -2,6 +2,7 @@ package com.ricardo.licenseservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 class LicenseServiceApplication {
     @LoadBalanced
     @Bean
